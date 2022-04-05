5ee8123eb9b61addc8ce3fdfde50186a44cdb33f
swbrock <90523707+swbrock@users.noreply.github.com>
2022-04-05 10:57:54 -0600

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UtahMotorVehicleAccidentAnalysis.Data;
+
+[assembly: HostingStartup(typeof(UtahMotorVehicleAccidentAnalysis.Areas.Identity.IdentityHostingStartup))]
+namespace UtahMotorVehicleAccidentAnalysis.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
