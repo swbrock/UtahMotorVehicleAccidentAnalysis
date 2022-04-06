cb031e47a23ada246165dc3c4ff02cb3caefd001
swbrock <90523707+swbrock@users.noreply.github.com>
2022-04-06 13:02:31 -0600

    helpers?

---
+++ Data/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace UtahMotorVehicleAccidentAnalysis.Data
+{
+    public class RDSContext : ApplicationDbContext
+    {
+        public RDSContext()
+            : base(GetRDSConnectionString())
+        {
+
+        }
+        public static RDSContext Create()
+        {
+            return new RDSContext();
+        }
+
+    }
+}
---
+++ Data/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace UtahMotorVehicleAccidentAnalysis.Data
+{
+    public class Helpers
+    {
+        public static string GetRDSConnectionString()
+        {
+            var appConfig = ConfigurationManager.AppSettings;
+
+            string dbname = appConfig["RDS_DB_NAME"];
+
+            if (string.IsNullOrEmpty(dbname)) return null;
+
+            string username = appConfig["admin"];
+            string password = appConfig["Group49!"];
+            string hostname = appConfig["accidents-db.crsciat1hkrn.us-west-1.rsd.amazonaws.com"];
+            string port = appConfig["3306"];
+
+            return "server=" + hostname + ";port=" + port + ";database=" + dbname + ";user=" + username + ";password=" + password + ";";
+        }
+    }
+}
