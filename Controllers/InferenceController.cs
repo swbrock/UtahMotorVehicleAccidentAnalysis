a893e711c38d9b120cdce9d318b312e5707155de
justiniantheemperor <justinrellis52@gmail.com>
2022-04-07 18:58:19 -0600

    add model and successfully compile

---
+++ Controllers/InferenceController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using UtahMotorVehicleAccidentAnalysis.Models;
+
+namespace aspnetcore.Controllers
+{ 
+    public class InferenceController : Controller
+    {
+        private InferenceSession _session;
+
+        public InferenceController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpGet]
+        public IActionResult EnterData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Score(CrashData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("input", data.AsTensor())
+            });
+            Tensor<long> score = result.First().AsTensor<long>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return View("Score", prediction);
+        }
+    }
+}
---
+++ Models/CrashData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtahMotorVehicleAccidentAnalysis.Models
+{
+    public class CrashData
+    {
+        public float pedestrian_involved_True { get; set; }
+        public float bicyclist_involved_True { get; set; }
+        public float motorcycle_involved_True { get; set; }
+        public float improper_restraint_True { get; set; }
+        public float unrestrained_True { get; set; }
+        public float dui_True { get; set; }
+        public float intersection_related_True { get; set; }
+        public float overturn_rollover_True { get; set; }
+        public float single_vehicle_True { get; set; }
+        public float distracted_driving_True { get; set; }
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+                pedestrian_involved_True,
+                bicyclist_involved_True,
+                motorcycle_involved_True,
+                improper_restraint_True,
+                unrestrained_True,
+                dui_True,
+                intersection_related_True,
+                overturn_rollover_True,
+                single_vehicle_True,
+                distracted_driving_True
+            };
+            int[] dimensions = new int[] { 1, 10 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+}
